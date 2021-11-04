99a736bd23ea5eb53196e02ec28ed5d59b390dcc
dh_lim_ <wawa4989@naver.com>
2021-11-04 17:13:30 +0900

    백준 개미3048 도전중

---
+++ ex/ex3048.java
@@ -0,0 +1,56 @@
+package ex;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ex3048 {
+
+	public static void main(String[] args) {
+		// 개미
+		//https://www.acmicpc.net/problem/3048
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N1=sc.nextInt();
+		int N2=sc.nextInt();
+		//System.out.println("N1: "+N1);
+		
+		String A= sc.next();
+		String B= sc.next();
+		
+		int T = sc.nextInt();
+		sc.close();
+		//System.out.println("string A: "+A);
+		
+		//A그룹은 문자열을 뒤집어야한다.
+		StringBuffer sb = new StringBuffer(A);
+		String A1 = sb.reverse().toString();
+		//System.out.println("A1: "+A1);
+		//String[] str= new String[A.length()+B.length()];
+		char[] str= new char[A1.length()+B.length()];
+		System.out.println("str배열: " +Arrays.toString(str));
+		
+		//배열로 따로따로 인덱스에 넣되, 하나의 배열로 합침
+		for(int i=0; i<str.length; i++) {
+			if(i<A1.length()) {
+				str[i] =A1.charAt(i);
+			}else {
+				str[i]=B.charAt(i-A1.length());
+			}
+		}
+		
+		System.out.println("str배열 정렬후: " +Arrays.toString(str));
+		
+		//자리를 바꿔봅시다.
+		int num=0;
+		char ch=' ';
+		String chrr= "";
+		for(int i=0; i<str.length; i++) {
+			chrr+=(char)str[i];
+			System.out.println("chrr: "+chrr);
+		}
+		
+
+	}
+
+}
