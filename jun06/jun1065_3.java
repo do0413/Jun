dae38304b91bc35d7876c7182bfb3d71b105f538
dh_lim_ <wawa4989@naver.com>
2021-11-19 20:41:15 +0900

    백준 문제 1065번

---
+++ jun06/jun1065_3.java
@@ -0,0 +1,15 @@
+package jun06;
+
+public class jun1065_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String url= "www.codechobo.com";
+		
+		System.out.printf("%s",url);
+		System.out.printf("%s%n",url);
+		System.out.printf("%s",url);
+		
+	}
+
+}
